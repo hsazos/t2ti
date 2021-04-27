a05cfc3323482b66c02c9584235636d8a1e9965c
hsazos <hfsazo@uc.cl>
2021-04-27 15:51:11 -0400

    Added Allowed HOst

--- myapi/settings.py
+++ myapi/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'k^$a^i33!3*2xt4#x0dhye^9oyo9@@9ab65rctxoehj#v4npso'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
