d8a4da27f8c0c87c4c05ce0a54178c798afd24ca
hsazos <hfsazo@uc.cl>
2021-04-27 16:24:04 -0400

    Update Settings

--- myapi/settings.py
+++ myapi/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k^$a^i33!3*2xt4#x0dhye^9oyo9@@9ab65rctxoehj#v4npso'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG_VALUE")
 
 ALLOWED_HOSTS = ['*']
 
